bb91e4a913b503d5d877ab725ffc89dd2f6861e1
paolotax <paolo.tassinari@gmail.com>
2024-02-17 22:53:16 +0100

    Update password confirmation label and configure email delivery method in development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
   # [RailsNotes UI Addition]
   # We use the Letter Opener gem for simple email previews in development
   #
-  #config.action_mailer.perform_deliveries = true
-  #config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
-  config.action_mailer.delivery_method = :resend
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.delivery_method = :letter_opener
 end
